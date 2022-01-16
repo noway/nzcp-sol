9ef347a62548b3fecf41e72058673ee9a18095d0
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-01-16 21:31:03 +1300

    verify signature from nzcp contract

--- contracts/nzcp.sol
+++ contracts/nzcp.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.11;
 import "hardhat/console.sol";
 import "./EllipticCurve.sol";
 
-contract NZCP {
+contract NZCP is EllipticCurve {
     string private greeting;
 
     string public constant x = "zRR-XGsCp12Vvbgui4DD6O6cqmhfPuXMhi1OxPl8760";
     string public constant y = "Iv5SU6FuW-TRYh5_GOrJlcV_gpF_GpFQhCOD8LSk3T0";
 
-    // constructor(string memory _greeting) {
-    //     console.log("Deploying a NZCP with greeting:", _greeting);
-    //     greeting = _greeting;
-    // }
+    constructor(string memory _greeting) {
+        console.log("Deploying a NZCP with greeting:", _greeting);
+        greeting = _greeting;
+    }
 
     function greet() public view returns (string memory) {
         return greeting;
@@ -32,7 +32,7 @@ contract NZCP {
     }
     
     function verifySignature(bytes32 message, uint[2] memory rs, uint[2] memory Q) public view returns (bool) {
-        bool isValid = EllipticCurve.validateSignature(message, rs, Q);
+        bool isValid = validateSignature(message, rs, Q);
         return isValid;
     }
 }
