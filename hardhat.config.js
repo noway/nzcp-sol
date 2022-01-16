28d78701eea76e3572faf26a305ed01a1690086f
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-01-16 21:06:30 +1300

    upgrade to solidity 0.8.11

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.11",
   paths: {
     sources: "./contracts",
     tests: "./test",
