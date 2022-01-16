99fa83fdfb2997c98bf2eb33ce4a2ae98d857c6d
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-01-16 20:24:34 +1300

    trying the test from EC repo

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,5 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const crypto         = require('crypto');
+const ecPem          = require('ec-pem');
+const ethereumJSUtil = require('ethereumjs-util');
 
 async function setupNZCP() {
   const NZCP = await ethers.getContractFactory("NZCP");
@@ -57,6 +60,7 @@ describe("NZCP", function () {
     expect(await nzcp.verifySignature(messageHash, r, s, v)).to.equal(true);
 
   })
+  /*
   it("Should verify signature with EC", async function () {
     const ec = await setupEC()
     const messageHash = "0x271CE33D671A2D3B816D788135F4343E14BC66802F8CD841FAAC939E8C11F3EE";
@@ -65,6 +69,48 @@ describe("NZCP", function () {
     const x = "0xCD147E5C6B02A75D95BDB82E8B80C3E8EE9CAA685F3EE5CC862D4EC4F97CEFAD";
     const y = "0x22FE5253A16E5BE4D1621E7F18EAC995C57F82917F1A9150842383F0B4A4DD3D";
     expect(await ec.validateSignature(messageHash, [r, s], [x, y])).to.equal(true);
+
+  })
+  */
+
+
+  it("Should verify signature with EC", async function () {
+
+    const ec = await setupEC()
+    // Create contract.
+    // curve = await SECP256R1.new();
+
+    // Create curve object for key and signature generation.
+    var prime256v1 = crypto.createECDH('prime256v1');
+    prime256v1.generateKeys();
+
+    // Reformat keys.
+    var pemFormattedKeyPair = ecPem(prime256v1, 'prime256v1');
+    const publicKey = [
+      '0x' + prime256v1.getPublicKey('hex').slice(2, 66),
+      '0x' + prime256v1.getPublicKey('hex').slice(-64)
+    ];
+
+    // Create random message and sha256-hash it.
+    var message = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    console.log('Message: ' + message);
+    const messageHash = ethereumJSUtil.bufferToHex(ethereumJSUtil.sha256(Buffer.from(message)));
+
+    // Create signature.
+    var signer = crypto.createSign('RSA-SHA256');
+    signer.update(message);
+    var sigString = signer.sign(pemFormattedKeyPair.encodePrivateKey(), 'hex');
+
+    // Reformat signature / extract coordinates.
+    var xlength = 2 * ('0x' + sigString.slice(6, 8));
+    var sigString = sigString.slice(8)
+    const signature = [
+      '0x' + sigString.slice(0, xlength),
+      '0x' + sigString.slice(xlength + 4)
+    ];
+
+
+    expect(await ec.validateSignature(messageHash, signature, publicKey)).to.equal(true);
 
   })
 
