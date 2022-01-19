2f914d6bee53bad769271ada1c90375951a80a06
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-01-19 22:47:34 +1300

    make needs a static variable

--- contracts/nzcp.sol
+++ contracts/nzcp.sol
@@ -21,6 +21,9 @@ contract NZCP is EllipticCurve {
     uint public constant LIVE_X = 0x0D008A26EB2A32C4F4BBB0A3A66863546907967DC0DDF4BE6B2787E0DBB9DAD7;
     uint public constant LIVE_Y = 0x971816CEC2ED548F1FA999933CFA3D9D9FA4CC6B3BC3B5CEF3EAD453AF0EC662;
 
+
+    string[] private needles = ["vc", "credentialSubject"];
+
     function memcpy(uint dest, uint src, uint len) private pure {
         // Copy word-length chunks while possible
         for(; len >= 32; len -= 32) {
@@ -171,7 +174,7 @@ contract NZCP is EllipticCurve {
         return (pos, map_len);
     }
 
-    function searchCBORTree(bytes memory buffer, uint pos, string[] memory needles, uint needle_pos) private view returns (uint) {
+    function searchCBORTree(bytes memory buffer, uint pos, uint needle_pos) private view returns (uint) {
         uint map_len;
         (pos, map_len) = readMapLength(buffer, pos);
 
@@ -194,13 +197,13 @@ contract NZCP is EllipticCurve {
                 (pos, key) = decodeString(buffer, pos, len);
                 // console.log("got to string!!", key);
                 if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked(needles[needle_pos]))) {
-                    if (needle_pos + 1 >= needles.length) {
+                    if (needle_pos + 1 >= needles.length) { // TODO: macro
                         // console.log("found all needles");
                         return pos;
                     }
                     else {
                         // console.log("about to parse:", needles[needle_pos]);
-                        return searchCBORTree(buffer, pos, needles, needle_pos + 1);
+                        return searchCBORTree(buffer, pos, needle_pos + 1);
                     }
                 }
                 else {
@@ -277,10 +280,7 @@ contract NZCP is EllipticCurve {
 
         uint skip = 27; // 27 bytes for ["Signature1", headers, buffer0] // TODO: macro
 
-        string[] memory needles = new string[](2);
-        needles[0] = "vc";
-        needles[1] = "credentialSubject";
-        uint credentialSubjectPos = searchCBORTree(buffer, skip, needles, 0);
+        uint credentialSubjectPos = searchCBORTree(buffer, skip, 0);
 
         string memory givenName;
         string memory familyName;
